4a99e66e4b24f5cddd7a74893e98ddb87c32b612
elysia <3302843873@qq.com>
2026-05-19 17:46:00 +0800

    addeadded flake

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      flake-utils,
+      nixpkgs,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        run-roxy-loader-c-template = pkgs.writeShellApplication {
+          name = "run-roxy-loader-c-template";
+          runtimeInputs = [
+            toolchain
+            pkgs.clang
+            pkgs.lld
+            pkgs.gnumake
+            pkgs.pkg-config
+            pkgs.qemu
+            pkgs.cacert
+          ];
+          text = ''
+            make run "$@"
+          '';
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            toolchain
+            pkgs.clang
+            pkgs.lld
+            pkgs.gnumake
+            pkgs.pkg-config
+            pkgs.qemu
+            pkgs.cacert
+          ];
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${run-roxy-loader-c-template}/bin/run-roxy-loader-c-template";
+        };
+      }
+    );
+}
