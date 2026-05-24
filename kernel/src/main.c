45205ef0ad4ee5a0822a14632c30f41c20768e90
elysia <3302843873@qq.com>
2026-05-24 13:38:45 +0800

    updated kernel to draw a straight line

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -4,6 +4,7 @@
 #include "roxy_loader.h"
 
 static void kernel_main(const BootInfo *bootinfo);
+static void put_pixel(const Framebuffer *framebuffer, size_t x, size_t y, uint32_t color);
 
 void _start(const BootInfo *bootinfo) {
     kernel_main(bootinfo);
@@ -13,10 +14,43 @@ void _start(const BootInfo *bootinfo) {
     }
 }
 
-static void kernel_main(const BootInfo *bootinfo) {
-    volatile uint8_t *framebuffer = (volatile uint8_t *)bootinfo->framebuffer.ptr;
+static void put_pixel(const Framebuffer *framebuffer, size_t x, size_t y, uint32_t color) {
+    size_t bytes_per_pixel = framebuffer->size / (framebuffer->stride * framebuffer->height);
+    size_t offset = (y * framebuffer->stride + x) * bytes_per_pixel;
+    volatile uint8_t *pixel = (volatile uint8_t *)framebuffer->ptr + offset;
 
-    for (size_t i = 0; i < bootinfo->framebuffer.size; i++) {
-        framebuffer[i] = 69;
+    switch (framebuffer->pixel_format) {
+        case Rgb:
+            pixel[0] = (uint8_t)((color >> 16) & 0xff);
+            pixel[1] = (uint8_t)((color >> 8) & 0xff);
+            pixel[2] = (uint8_t)(color & 0xff);
+            break;
+        case Bgr:
+            pixel[0] = (uint8_t)(color & 0xff);
+            pixel[1] = (uint8_t)((color >> 8) & 0xff);
+            pixel[2] = (uint8_t)((color >> 16) & 0xff);
+            break;
+        default:
+            pixel[0] = (uint8_t)(color & 0xff);
+            pixel[1] = (uint8_t)((color >> 8) & 0xff);
+            pixel[2] = (uint8_t)((color >> 16) & 0xff);
+            break;
+    }
+}
+
+static void kernel_main(const BootInfo *bootinfo) {
+    const Framebuffer *framebuffer = &bootinfo->framebuffer;
+    uint32_t background = 0x101820;
+    uint32_t line = 0xffd166;
+
+    for (size_t y = 0; y < framebuffer->height; y++) {
+        for (size_t x = 0; x < framebuffer->width; x++) {
+            put_pixel(framebuffer, x, y, background);
+        }
+    }
+
+    for (size_t x = 0; x < framebuffer->width; x++) {
+        size_t y = x * framebuffer->height / framebuffer->width;
+        put_pixel(framebuffer, x, y, line);
     }
 }
