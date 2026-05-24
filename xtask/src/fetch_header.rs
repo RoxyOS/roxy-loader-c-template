26dbff0cd79eea00f7e452989f1664ca40bc1a05
elysia <3302843873@qq.com>
2026-05-24 13:43:34 +0800

    lock ref when fetching headers at v0.2.3

--- xtask/src/fetch_header.rs
+++ xtask/src/fetch_header.rs
@@ -4,8 +4,8 @@ use anyhow::{Context, Result};
 
 use crate::utils::downloaded_header_path;
 
-const HEADER_URL: &str =
-    "https://raw.githubusercontent.com/RoxyOS/roxy-loader/main/c_api/roxy_loader.h";
+const ROXY_LOADER_REF: &str = "v0.2.3";
+const HEADER_PATH: &str = "c_api/roxy_loader.h";
 
 pub fn fetch_header() -> Result<PathBuf> {
     let output = downloaded_header_path()?;
@@ -15,9 +15,12 @@ pub fn fetch_header() -> Result<PathBuf> {
 
     std::fs::create_dir_all(parent)?;
 
-    let response = ureq::get(HEADER_URL)
+    let header_url =
+        format!("https://raw.githubusercontent.com/RoxyOS/roxy-loader/{ROXY_LOADER_REF}/{HEADER_PATH}");
+
+    let response = ureq::get(&header_url)
         .call()
-        .with_context(|| format!("failed to download header from {HEADER_URL}"))?;
+        .with_context(|| format!("failed to download header from {header_url}"))?;
     let mut body = response.into_body();
     let header = body
         .read_to_string()
